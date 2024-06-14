243f904cb9d71fe80adb9e9afca7261ac0fbee63
babystrawberry <118613135+babystrawberry@users.noreply.github.com>
2024-06-14 15:22:04 -0400

    Update bills.py

--- Bills Bot/bills.py
+++ Bills Bot/bills.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv(r'/Users/sahanasantosh/Documents/Bills Bot/bills - Sheet1.csv')
+df = pd.read_csv(r'bills - Sheet1.csv')
 df['doc-number'] = df['doc-number'].str.replace(' ', '', regex=False)
 df['Sponsors'] = df['Sponsors'].str.replace(r'(\d+(?:st|nd|rd|th))', r'\1\n', regex=True)
 df['Committees'] = df['Committees'].str.replace(r'(House Committee:.*?)(Senate Committee:)', r'\1\n\2', regex=True)
